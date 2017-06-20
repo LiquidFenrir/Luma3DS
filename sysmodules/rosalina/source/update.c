2201e5969e60a9a8d047052b358a978136323ee6
Alex Taber <alexftaber@gmail.com>
2017-06-20 16:50:12 -0400

    Revert "THANK FUCK"
    This reverts commit 1eaf81ec72e808d3a6dd63e4bfce85eed82ccfdf.

--- sysmodules/rosalina/source/update.c
+++ sysmodules/rosalina/source/update.c
@@ -2,7 +2,6 @@
 #include "update.h"
 #include "menu.h"
 #include "memory.h"
-#include "fmt.h"
 
 static MyThread updateThread;
 static u8 ALIGN(8) updateThreadStack[THREAD_STACK_SIZE];
@@ -14,13 +13,6 @@ MyThread *updateCreateThread(void)
 	return	&updateThread;
 }
 
-// Thanks Jess <3
-void AtoW(char* in, u16* out) {
-	int i;
-    for (i = 0; i < strlen(in); i++)
-    	out[i] = in[i];
-}
-
 void updateThreadMain(void)
 {
 	newsInit();
@@ -28,21 +20,7 @@ void updateThreadMain(void)
 	{
 		if ((HID_PAD & (BUTTON_UP | BUTTON_R1 | BUTTON_START)) == (BUTTON_UP | BUTTON_R1 | BUTTON_START))
 		{
-			char messageString[128];
-
-			s64 out;
-			u32 version;
-
-			svcGetSystemInfo(&out, 0x10000, 0);
-			version = (u32)out;
-
-			sprintf(messageString, "Current Luma Version: v%u.%u.%u", GET_VERSION_MAJOR(version), GET_VERSION_MINOR(version), GET_VERSION_REVISION(version));
-
-			u16 messageBytes[256];
-
-			AtoW(messageString, messageBytes);
-
-			NEWS_AddNotification(u"Test", 4, messageBytes, strlen(messageString), NULL, 0, false);
+			NEWS_AddNotification(u"test", 4, u"test test", 9, NULL, 0, false);
 		}
         svcSleepThread(50 * 1000 * 1000LL);
 	}
