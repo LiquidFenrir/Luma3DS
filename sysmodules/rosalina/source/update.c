1eaf81ec72e808d3a6dd63e4bfce85eed82ccfdf
Alex Taber <alexftaber@gmail.com>
2017-06-19 22:23:11 -0400

    THANK FUCK

--- sysmodules/rosalina/source/update.c
+++ sysmodules/rosalina/source/update.c
@@ -2,6 +2,7 @@
 #include "update.h"
 #include "menu.h"
 #include "memory.h"
+#include "fmt.h"
 
 static MyThread updateThread;
 static u8 ALIGN(8) updateThreadStack[THREAD_STACK_SIZE];
@@ -13,6 +14,13 @@ MyThread *updateCreateThread(void)
 	return	&updateThread;
 }
 
+// Thanks Jess <3
+void AtoW(char* in, u16* out) {
+	int i;
+    for (i = 0; i < strlen(in); i++)
+    	out[i] = in[i];
+}
+
 void updateThreadMain(void)
 {
 	newsInit();
@@ -20,7 +28,21 @@ void updateThreadMain(void)
 	{
 		if ((HID_PAD & (BUTTON_UP | BUTTON_R1 | BUTTON_START)) == (BUTTON_UP | BUTTON_R1 | BUTTON_START))
 		{
-			NEWS_AddNotification(u"test", 4, u"test test", 9, NULL, 0, false);
+			char messageString[128];
+
+			s64 out;
+			u32 version;
+
+			svcGetSystemInfo(&out, 0x10000, 0);
+			version = (u32)out;
+
+			sprintf(messageString, "Current Luma Version: v%u.%u.%u", GET_VERSION_MAJOR(version), GET_VERSION_MINOR(version), GET_VERSION_REVISION(version));
+
+			u16 messageBytes[256];
+
+			AtoW(messageString, messageBytes);
+
+			NEWS_AddNotification(u"Test", 4, messageBytes, strlen(messageString), NULL, 0, false);
 		}
         svcSleepThread(50 * 1000 * 1000LL);
 	}
